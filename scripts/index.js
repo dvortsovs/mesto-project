4c1be3cf77f30eb3a892e7c9a0d47056b68cd47d
Sevastian Dvortsov <fane28@mail.ru>
2021-10-07 17:34:59 +0300

    logic for save button and value input form

--- scripts/index.js
+++ scripts/index.js
@@ -1,12 +1,27 @@
 const container = document.querySelector('.page');
 const editButton = container.querySelector('.profile__edit-button');
+const saveButton = container.querySelector('.popup__save-button');
 const closeButton = container.querySelector('.popup__close-button');
 const editPopup = container.querySelector('.popup');
+const profileName = container.querySelector('.profile__name');
+const profileCaption = container.querySelector('.profile__caption');
+const inputName = container.querySelector('input[name=name]');
+const inputCaption = container.querySelector('input[name=caption]');
+
+function saveProfile() {
+  profileName.textContent = inputName.value;
+  profileCaption.textContent = inputCaption.value;
+  showHidePopup(editPopup);
+}
 
 function showHidePopup(popup) {
-  console.log(popup)
   popup.classList.toggle('popup_opened');
 }
 
-editButton.addEventListener('click', () => showHidePopup(editPopup));
-closeButton.addEventListener('click', () => showHidePopup(editPopup))
+editButton.addEventListener('click', function () {
+  inputName.value = profileName.textContent;
+  inputCaption.value = profileCaption.textContent;
+  showHidePopup(editPopup);
+});
+closeButton.addEventListener('click', () => showHidePopup(editPopup));
+saveButton.addEventListener('click', () => saveProfile());
