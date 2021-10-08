c1b89584b657e91de01edaf57f97e1a5dc286579
Sevastian Dvortsov <fane28@mail.ru>
2021-10-08 13:29:52 +0300

    add new addPopup

--- scripts/index.js
+++ scripts/index.js
@@ -1,23 +1,35 @@
 const container = document.querySelector('.page');
 const editButton = container.querySelector('.profile__edit-button');
 const saveButton = container.querySelector('.popup__save-button');
-const closeButton = container.querySelector('.popup__close-button');
-const editPopup = container.querySelector('.popup');
+const addButton = container.querySelector('.profile__add-button');
+const popupList = container.querySelectorAll('.popup');
+const editPopup = popupList[0];
+const addPopup = popupList[1];
+const closeEditPopup = popupList[0].querySelector('.popup__close-button');
+const closeAddPopup = popupList[1].querySelector('.popup__close-button');
 const profileName = container.querySelector('.profile__name');
 const profileCaption = container.querySelector('.profile__caption');
-const formElement = container.querySelector('form[name=edit-profile]');
-const inputName = formElement.querySelector('input[name=name]');
-const inputCaption = formElement.querySelector('input[name=caption]');
+const editForm = popupList[0].querySelector('.popup__form');
+const inputName = editForm.querySelector('input[name=name]');
+const inputCaption = editForm.querySelector('input[name=caption]');
+const addForm = popupList[1].querySelector('.popup__form');
+const inputTitle = addForm.querySelector('input[name=title]');
+const inputLink = addForm.querySelector('input[name=link]');
 
 function showHidePopup(popup) {
   popup.classList.toggle('popup_opened');
 }
 
-function formSubmitHandler(evt) {
+function editFormSubmitHandler(evt) {
   evt.preventDefault();
   profileName.textContent = inputName.value;
   profileCaption.textContent = inputCaption.value;
   showHidePopup(editPopup);
+}
+
+function addFormSubmitHandler(evt) {
+  evt.preventDefault();
+  showHidePopup(addPopup);
 }
 
 editButton.addEventListener('click', function () {
@@ -25,5 +37,9 @@ editButton.addEventListener('click', function () {
   inputCaption.value = profileCaption.textContent;
   showHidePopup(editPopup);
 });
-closeButton.addEventListener('click', () => showHidePopup(editPopup));
-formElement.addEventListener('submit', formSubmitHandler);
+
+addButton.addEventListener('click', () => showHidePopup(addPopup));
+closeEditPopup.addEventListener('click', () => showHidePopup(editPopup));
+closeAddPopup.addEventListener('click', () => showHidePopup(addPopup));
+editForm.addEventListener('submit', editFormSubmitHandler);
+addForm.addEventListener('submit', addFormSubmitHandler);
