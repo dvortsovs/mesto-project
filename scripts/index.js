1aefe071491814ecac621fde9386828dff5b22a5
Sevastian Dvortsov <fane28@mail.ru>
2021-10-14 18:24:59 +0300

    card image resolution fix add like

--- scripts/index.js
+++ scripts/index.js
@@ -1,30 +1,3 @@
-const initialCards = [
-  {
-    name: 'Архыз',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
-  },
-  {
-    name: 'Челябинская область',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
-  },
-  {
-    name: 'Иваново',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
-  },
-  {
-    name: 'Камчатка',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
-  },
-  {
-    name: 'Холмогорский район',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
-  },
-  {
-    name: 'Байкал',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-  }
-];
-
 const container = document.querySelector('.page');
 const elementsList = container.querySelector('.elements__list')
 const editButton = container.querySelector('.profile__edit-button');
@@ -50,7 +23,9 @@ function addCard(title, src) {
 
   cardElement.querySelector('.content-card__title').textContent = title;
   cardElement.querySelector('.content-card__image').src = src;
-
+  cardElement.querySelector('.content-card__like-button').addEventListener('click', function (evt) {
+  evt.target.classList.toggle('content-card__like-button_active')
+});
   elementsList.prepend(cardElement);
 }
 
