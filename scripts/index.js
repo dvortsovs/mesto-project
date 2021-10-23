f4837567bd55e0292cc1c8facfc41ce092243367
Sevastian Dvortsov <fane28@mail.ru>
2021-10-23 16:14:10 +0300

    review edit

--- scripts/index.js
+++ scripts/index.js
@@ -17,8 +17,8 @@ const inputCaption = editForm.querySelector('.popup__input_type_caption');
 const addForm = addPopup.querySelector('.popup__form');
 const inputTitle = addForm.querySelector('.popup__input_type_title');
 const inputLink = addForm.querySelector('.popup__input_type_link');
-let srcPopupImage = imagePopup.querySelector('.popup__image');
-let titlePopupImage = imagePopup.querySelector('.popup__title');
+const srcPopupImage = imagePopup.querySelector('.popup__image');
+const titlePopupImage = imagePopup.querySelector('.popup__title');
 
 function addCard(title, src) {
   const cardTemplate = container.querySelector('#content-card').content;
@@ -29,32 +29,38 @@ function addCard(title, src) {
   cardElement.querySelector('.content-card__image').addEventListener('click', function () {
     srcPopupImage.src = src;
     titlePopupImage.textContent = title;
-    showHidePopup(imagePopup);
+    showPopup(imagePopup);
   });
   cardElement.querySelector('.content-card__like-button').addEventListener('click', function (evt) {
     evt.target.classList.toggle('content-card__like-button_active');
 });
-  cardElement.querySelector('.content-card__delete-button').addEventListener('click', function (evt) {
-    evt.target.parentElement.remove();
-  });
-  elementsList.prepend(cardElement);
+  cardElement.querySelector('.content-card__delete-button').addEventListener('click', () => cardElement.remove());
+  return cardElement;
 }
 
-function showHidePopup(popup) {
-  popup.classList.toggle('popup_opened');
+function renderCard(card) {
+  elementsList.prepend(card);
+}
+
+function showPopup(popup) {
+  popup.classList.add('popup_opened');
+}
+
+function hidePopup(popup) {
+  popup.classList.remove('popup_opened');
 }
 
 function editFormSubmitHandler(evt) {
   evt.preventDefault();
   profileName.textContent = inputName.value;
   profileCaption.textContent = inputCaption.value;
-  showHidePopup(editPopup);
+  hidePopup(editPopup);
 }
 
 function addFormSubmitHandler(evt) {
   evt.preventDefault();
-  addCard(inputTitle.value, inputLink.value);
-  showHidePopup(addPopup);
+  renderCard(addCard(inputTitle.value, inputLink.value));
+  hidePopup(addPopup);
   inputTitle.value = '';
   inputLink.value = '';
 }
@@ -62,14 +68,14 @@ function addFormSubmitHandler(evt) {
 editButton.addEventListener('click', function () {
   inputName.value = profileName.textContent;
   inputCaption.value = profileCaption.textContent;
-  showHidePopup(editPopup);
+  showPopup(editPopup);
 });
 
-initialCards.reverse().forEach(item => addCard(item.name, item.link));
+initialCards.reverse().forEach(item => renderCard(addCard(item.name, item.link)));
 
-addButton.addEventListener('click', () => showHidePopup(addPopup));
-closeEditPopup.addEventListener('click', () => showHidePopup(editPopup));
-closeAddPopup.addEventListener('click', () => showHidePopup(addPopup));
-closeImagePopup.addEventListener('click', () => showHidePopup(imagePopup));
+addButton.addEventListener('click', () => showPopup(addPopup));
+closeEditPopup.addEventListener('click', () => hidePopup(editPopup));
+closeAddPopup.addEventListener('click', () => hidePopup(addPopup));
+closeImagePopup.addEventListener('click', () => hidePopup(imagePopup));
 editForm.addEventListener('submit', editFormSubmitHandler);
 addForm.addEventListener('submit', addFormSubmitHandler);
