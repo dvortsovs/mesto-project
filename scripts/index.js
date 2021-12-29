de380b7196ec4f097d7345c5397463a0efaf9c70
Sevastian Dvortsov <86145076+dvortsovs@users.noreply.github.com>
2021-12-29 16:41:26 +0300

    feat: add form validate

--- scripts/index.js
+++ scripts/index.js
@@ -65,15 +65,84 @@ function addFormSubmitHandler(evt) {
   inputLink.value = '';
 }
 
+function showInputError(formElement, inputElement, errorMessage) {
+  const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+  inputElement.classList.add('popup__input_type_error');
+  errorElement.textContent = errorMessage;
+  errorElement.classList.add('popup__input-error_active');
+}
+
+function hideInputError(formElement, inputElement) {
+  const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+  inputElement.classList.remove('popup__input_type_error');
+  errorElement.classList.remove('popup__input-error_active');
+  errorElement.textContent = '';
+}
+
+function checkInputValidity(formElement, inputElement) {
+  if (!inputElement.validity.valid) {
+    showInputError(formElement, inputElement, inputElement.validationMessage);
+  } else {
+    hideInputError(formElement, inputElement);
+  }
+}
+
+function hasInvalidInput(inputList) {
+  return inputList.some((inputElement) => {
+    return !inputElement.validity.valid;
+  });
+}
+
+function toggleButtonState(inputList, buttonElement) {
+  if(hasInvalidInput(inputList)) {
+    buttonElement.classList.add('popup__save-button_disabled');
+    buttonElement.setAttribute('disabled', true);
+  } else {
+    buttonElement.classList.remove('popup__save-button_disabled');
+    buttonElement.removeAttribute('disabled');
+  }
+}
+
+function setEventListeners(formElement) {
+  const inputList = Array.from(formElement.querySelectorAll('.popup__input'));
+  const buttonElement = formElement.querySelector('.popup__save-button');
+  toggleButtonState(inputList, buttonElement);
+  inputList.forEach((inputElement) => {
+    inputElement.addEventListener('input', function () {
+      checkInputValidity(formElement, inputElement);
+      toggleButtonState(inputList, buttonElement);
+    });
+  });
+}
+
+function enableValidation(popup) {
+  const formList = Array.from(popup.querySelectorAll('.popup__form'));
+  formList.forEach((formElement) => {
+    formElement.addEventListener('submit', function (evt) {
+      evt.preventDefault();
+    });
+    const fieldsetList = Array.from(formElement.querySelectorAll('.popup__input-container'));
+    fieldsetList.forEach((fieldSet) => {
+      setEventListeners(fieldSet);
+    });
+  });
+}
+
+
+
+initialCards.reverse().forEach(item => renderCard(addCard(item.name, item.link)));
+
 editButton.addEventListener('click', function () {
   inputName.value = profileName.textContent;
   inputCaption.value = profileCaption.textContent;
   showPopup(editPopup);
+  enableValidation(editPopup);
+});
+addButton.addEventListener('click', () => {
+  showPopup(addPopup);
+  enableValidation(addPopup);
 });
 
-initialCards.reverse().forEach(item => renderCard(addCard(item.name, item.link)));
-
-addButton.addEventListener('click', () => showPopup(addPopup));
 closeEditPopup.addEventListener('click', () => hidePopup(editPopup));
 closeAddPopup.addEventListener('click', () => hidePopup(addPopup));
 closeImagePopup.addEventListener('click', () => hidePopup(imagePopup));
