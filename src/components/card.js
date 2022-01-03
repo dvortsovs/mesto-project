cfca15fc8260a602914fbf63a7903769ed110268
Sevastian Dvortsov <86145076+dvortsovs@users.noreply.github.com>
2022-01-03 12:09:41 +0300

    fix: refactor code for review

--- src/components/card.js
+++ src/components/card.js
@@ -3,17 +3,31 @@ const imagePopup = document.querySelector('.popup_type_image');
 const srcPopupImage = imagePopup.querySelector('.popup__image');
 const titlePopupImage = imagePopup.querySelector('.popup__title');
 
-function addCard(title, src, showPopup, escClose) {
+function defineAltValue(src) {
+  let result = '';
+  const arr = src.split('').reverse();
+  for (let i = 0; i < src.length; i++) {
+    if (arr[i] === '/') {
+      break
+    } else {
+      result += arr[i];
+    }
+  }
+  return result.split('').reverse().join('')
+}
+
+function addCard(title, src, showPopup) {
   const cardTemplate = document.querySelector('#content-card').content;
   const cardElement = cardTemplate.querySelector('.content-card').cloneNode(true);
+  const cardImage = cardElement.querySelector('.content-card__image');
 
   cardElement.querySelector('.content-card__title').textContent = title;
-  cardElement.querySelector('.content-card__image').src = src;
-  cardElement.querySelector('.content-card__image').addEventListener('click', function () {
+  cardImage.src = src;
+  cardImage.alt = defineAltValue(src)
+  cardImage.addEventListener('click', function () {
     srcPopupImage.src = src;
     titlePopupImage.textContent = title;
     showPopup(imagePopup);
-    document.addEventListener('keydown', escClose)
   });
   cardElement.querySelector('.content-card__like-button').addEventListener('click', function (evt) {
     evt.target.classList.toggle('content-card__like-button_active');
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,13 @@
 import './index.css';
-import {enableValidation} from "../components/validate";
+import {enableValidation, toggleButtonState} from "../components/validate";
 import {initialCards, validateConfig} from "../components/constants";
-import {renderCard, addCard, imagePopup} from "../components/card";
+import {renderCard, addCard} from "../components/card";
 import {
   showPopup,
   hidePopup,
-  editFormSubmitHandler,
-  addFormSubmitHandler,
-  escClose,
-  overlayClose,
-  closeAddPopup,
-  closeImagePopup,
-  closeEditPopup,
+  handleEditFormSubmit,
+  handleAddFormSubmit,
+  popups,
   addPopup,
   editPopup,
   addForm,
@@ -26,30 +22,34 @@ const container = document.querySelector('.page');
 const editButton = container.querySelector('.profile__edit-button');
 const addButton = container.querySelector('.profile__add-button');
 
-
-initialCards.reverse().forEach(item => renderCard(addCard(item.name, item.link, showPopup, escClose)));
+initialCards.reverse().forEach(item => renderCard(addCard(item.name, item.link, showPopup)));
 
 enableValidation(validateConfig);
+
+popups.forEach((popup) => {
+  popup.addEventListener('mousedown', (evt) => {
+    if (evt.target.classList.contains('popup_opened')) {
+      hidePopup(popup);
+    }
+    if (evt.target.classList.contains('popup__close-button')) {
+      hidePopup(popup);
+    }
+  });
+});
 
 editButton.addEventListener('click', function () {
   inputName.value = profileName.textContent;
   inputCaption.value = profileCaption.textContent;
   showPopup(editPopup);
-  document.addEventListener('keydown', escClose);
 });
 
 addButton.addEventListener('click', function () {
+  const form = document.forms.add;
+  const inputList = Array.from(form.querySelectorAll(validateConfig.inputSelector));
+  const buttonElement = form.querySelector(validateConfig.submitButtonSelector);
+  toggleButtonState(inputList, buttonElement, validateConfig);
   showPopup(addPopup);
-  document.addEventListener('keydown', escClose);
 });
 
-editPopup.addEventListener('mousedown', overlayClose);
-addPopup.addEventListener('mousedown', overlayClose);
-imagePopup.addEventListener('mousedown', overlayClose);
-
-
-closeEditPopup.addEventListener('click', () => hidePopup(editPopup));
-closeAddPopup.addEventListener('click', () => hidePopup(addPopup));
-closeImagePopup.addEventListener('click', () => hidePopup(imagePopup));
-editForm.addEventListener('submit', editFormSubmitHandler);
-addForm.addEventListener('submit', (evt) => addFormSubmitHandler(evt, renderCard, addCard));
+editForm.addEventListener('submit', handleEditFormSubmit);
+addForm.addEventListener('submit', (evt) => handleAddFormSubmit(evt, renderCard, addCard, showPopup));
